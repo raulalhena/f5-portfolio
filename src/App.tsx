b552660a13f575bab44139d2a0b13c2078060b04
Raul <raul.alhena@gmail.com>
2023-08-08 12:55:55 +0200

    routes done

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+import Hero from "./pages/Hero";
+import Home from "./pages/Home";
+import Aboutme from "./pages/Aboutme";
+import Technologies from "./pages/Technologies";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Hero />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/about' element={<Aboutme />} />
+        <Route path='/tech' element={<Technologies />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div>Are you lost? Don't worry, follow the white rabbit!</div>
+  )
+}
+
+export default NotFound
