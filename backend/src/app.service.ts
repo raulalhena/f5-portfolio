5a03dd43703f5861179d6f639de2f71f09934fdf
Raul <raul.alhena@gmail.com>
2023-09-13 14:57:02 +0200

    changed message

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Back deploy working';
+    return 'Back deploy working 2';
   }
 }
